65523e80e5a54135b5c639576cfa6e60c035e292
thecheckgaur <140734672+thecheckgaur@users.noreply.github.com>
2023-08-08 09:39:20 +0530

    Add files via upload
    can check odd or even numbers with the help of js.

---
+++ Odd & Even Checker/java.js
@@ -0,0 +1,7 @@
+var num = parseInt(prompt("Enter the Number to Check"))
+if(num%2==0){
+    document.write(`Yes, ${num} is an Even Number`)
+}
+else{
+    document.write(`No, ${num} is an Odd Number`)
+}
